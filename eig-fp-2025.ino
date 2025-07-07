3bf73385c58db82e5f64f8ae0276e301297e950b
tyzrichard <tyzrichard28@gmail.com>
2025-07-07 23:05:47 +0800

    Changed pins so none of the bootstrap pins are affected

--- eig-fp-2025.ino
+++ eig-fp-2025.ino
@@ -1,14 +1,13 @@
-
 #include <BleCombo.h>
 
-const int PB = 14;
-const int JOY_X = 4, JOY_Y = 0, JOY_SW = 34;
+int PB = 14;   // Change
+int JOY_X = 33, JOY_Y = 4, JOY_SW = 34;  // Change
+int x_value, y_value;                     // Change
 
+// VCC laser, pb, joystick
+// GND laser, joystick
+// data pb
 
-const int x_left_trigger = 1750,
-          x_right_trigger = 2100,
-          y_top_trigger = 3100,
-          y_bottom_trigger = 3500;  // Joystick trigger boundaries
 int JOY_SW_State = LOW;
 int last_JOY_SW_State = LOW;
 unsigned long lastJOYDebounceTime = 0;
@@ -16,33 +15,32 @@ int PB_State = LOW;
 int last_PB_State = LOW;
 unsigned long lastPBDebounceTime = 0;
 unsigned long debounceDelay = 50;  // 50 ms debounce delay
-
-String deviceName = "Klickr 9000 Pro";
-String manufacturer = "richard";
+// "Klickr 9000."
 
 void setup() {
   pinMode(PB, INPUT_PULLDOWN);
+
   pinMode(JOY_X, INPUT);
   pinMode(JOY_Y, INPUT);
   pinMode(JOY_SW, INPUT_PULLDOWN);
-  pinMode(LED_BUILTIN, OUTPUT);
 
+  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-
-  initDevice(deviceName, manufacturer);
   Serial.begin(921600);
+
+  Keyboard.end();
+  Mouse.end();
+  Serial.println("Starting device...");
+  Keyboard.begin();
+  Mouse.begin();
 }
 
 void loop() {
-  if (Keyboard.isConnected()) {
-    pushbutton();
-    joystick();
-  } else {
-    Serial.println("Disconnected");
-    delay(1000);
-    initDevice(deviceName, manufacturer);
-  }
+  Serial.print("Pushbutton status - ");
+  Serial.println(digitalRead(PB));
+  pushbutton(); // comment out if using wasd
+  joystick();               
 }
 
 void pushbutton() {
@@ -57,7 +55,7 @@ void pushbutton() {
       PB_State = reading;
 
       if (PB_State == HIGH) {
-        Keyboard.print("Hello world!");  // Assignable macro
+        Keyboard.print("Hello world!");
       }
     }
   }
@@ -66,20 +64,16 @@ void pushbutton() {
 }
 
 void joystick() {
-  joystickRead();
-  // Mouse or WASD - Choose ONE
-  mouse();
-  // wasd();
-}
-
-int x_value, y_value;
-
-void joystickRead() {
   x_value = analogRead(JOY_X);
   y_value = analogRead(JOY_Y);
   Serial.println("X Value: " + String(x_value) + " | Y Value: " + String(y_value));
+
+  // Mouse or WASD - Choose ONE
+  mouse();
+  //wasd();
 }
 
+int x_left_trigger = 300, x_right_trigger = 3800, y_top_trigger = 300, y_bottom_trigger = 3800; // Joystick trigger boundaries
 
 void mouse() {
   int x = analogRead(JOY_X);
@@ -88,10 +82,10 @@ void mouse() {
 
   int dx = 0, dy = 0;
 
-  if (x < x_left_trigger) dx = -1;   // move left
-  if (x > x_right_trigger) dx = 1;   // move right
-  if (y < y_top_trigger) dy = -1;    //move up
-  if (y > y_bottom_trigger) dy = 1;  // move down
+  if (x < x_left_trigger) dx = -1; // move left
+  if (x > x_right_trigger) dx = 1; // move right
+  if (y < y_top_trigger) dy = -1; //move up
+  if (y > y_bottom_trigger) dy = 1; // move down
 
   if (dx != 0 || dy != 0) {
     Mouse.move(dx, dy, 0);  // move mouse
@@ -117,60 +111,43 @@ void mouse() {
   last_JOY_SW_State = reading;
 }
 
+bool wPressed = false;
+bool aPressed = false;
+bool sPressed = false;
+bool dPressed = false;
+bool spacePressed = false;
 
 void wasd() {
-
-  bool wPressed = false;
-  bool aPressed = false;
-  bool sPressed = false;
-  bool dPressed = false;
-  bool spacePressed = false;
   int x = analogRead(JOY_X);
   int y = analogRead(JOY_Y);
   bool btn = digitalRead(PB) == HIGH;
 
   // --- Handle W (UP) ---
   if (y < y_top_trigger) {
-    if (!wPressed) {
-      Keyboard.press('w');
-      wPressed = true;
-    }
+    if (!wPressed) { Keyboard.press('w'); wPressed = true; }
   } else if (wPressed) {
-    Keyboard.release('w');
-    wPressed = false;
+    Keyboard.release('w'); wPressed = false;
   }
 
   // --- Handle S (DOWN) ---
   if (y > y_bottom_trigger) {
-    if (!sPressed) {
-      Keyboard.press('s');
-      sPressed = true;
-    }
+    if (!sPressed) { Keyboard.press('s'); sPressed = true; }
   } else if (sPressed) {
-    Keyboard.release('s');
-    sPressed = false;
+    Keyboard.release('s'); sPressed = false;
   }
 
   // --- Handle A (LEFT) ---
   if (x < x_left_trigger) {
-    if (!aPressed) {
-      Keyboard.press('a');
-      aPressed = true;
-    }
+    if (!aPressed) { Keyboard.press('a'); aPressed = true; }
   } else if (aPressed) {
-    Keyboard.release('a');
-    aPressed = false;
+    Keyboard.release('a'); aPressed = false;
   }
 
   // --- Handle D (RIGHT) ---
   if (x > x_right_trigger) {
-    if (!dPressed) {
-      Keyboard.press('d');
-      dPressed = true;
-    }
+    if (!dPressed) { Keyboard.press('d'); dPressed = true; }
   } else if (dPressed) {
-    Keyboard.release('d');
-    dPressed = false;
+    Keyboard.release('d'); dPressed = false;
   }
 
   // --- Handle SPACEBAR button ---
@@ -181,17 +158,6 @@ void wasd() {
     Keyboard.release(' ');
     spacePressed = false;
   }
-  delay(1000 / x);  // debounce/polling delay
-}
 
-void initDevice(String deviceName, String deviceManufacturer) {
-  Keyboard.end();
-  Mouse.end();
-  Keyboard.setDeviceName(deviceName);
-  Keyboard.setDeviceManufacturer(deviceManufacturer);
-  Serial.println("Starting device...");
-  Keyboard.begin();
-  Mouse.begin();
-  if (Keyboard.isConnected())
-    Serial.println("Connected");
+  delay(10); // debounce/polling delay
 }
